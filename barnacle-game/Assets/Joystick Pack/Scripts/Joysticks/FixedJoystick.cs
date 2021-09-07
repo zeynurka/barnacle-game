eefdd45004bc39f436c3812b85c097a9e240d860
zeynurka <zeynepnurbanu.karaman@gazi.edu.tr>
2021-09-07 16:25:21 +0300

    guncelleme

--- barnacle-game/Assets/Joystick Pack/Scripts/Joysticks/FixedJoystick.cs
+++ barnacle-game/Assets/Joystick Pack/Scripts/Joysticks/FixedJoystick.cs
@@ -1,8 +1,30 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class FixedJoystick : Joystick
+public class FixedJoystick : MonoBehaviour, IPointerUpHandler, IPointerDownHandler
 {
+    [HideInInspector]
+    public bool Pressed;
 
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Pressed = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Pressed = false;
+    }
 }
--- barnacle-game/Assets/PlayerController.cs
+++ barnacle-game/Assets/PlayerController.cs
@@ -1,18 +1,39 @@
+﻿using System.Collections.Generic;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    Animator animator;
+
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        //çalışıp çalışmadığını kontrol etmek için
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            animator.SetTrigger("Falling");
+        }
+
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        bool fire = Input.GetButtonDown("Fire1");
+
+        animator.SetFloat("Forward", v);
+        animator.SetFloat("Strafe", h);
+        animator.SetBool("Fire", fire);
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.CompareTag("Enemy"))
+        {
+            animator.SetTrigger("Falling");
+        }
     }
 }
