9e5fc23e548fd85b35cb5dedde05dd57358b3403
DervisG <dervisgurler@gmail.com>
2021-09-08 11:45:13 +0300

    notlar

--- barnacle-game/Assets/PlayerController.cs
+++ barnacle-game/Assets/PlayerController.cs
@@ -8,7 +8,7 @@ public class PlayerController : MonoBehaviour
 {
 
     Animator animator;
-    Joystick joystick;
+    Joystick joystick;//Bu ne inspector tarafından ne kod tarafından tanımlanıyor. Bu tanımlı olmadığı için xAxis yAxis cekemiyor. 
 
     void Start()
     {
@@ -17,11 +17,11 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        animator.SetBool("walking", true);
+        animator.SetBool("walking", true);// update sürekli calıstığı için bunu böyle yazmak doğru değil
 
         float xAxis = joystick.Horizontal;
         float yAxis = joystick.Vertical;
-        Vector3 direction = new Vector3(xAxis, 0f, yAxis).normalized;
+        Vector3 direction = new Vector3(xAxis, 0f, yAxis).normalized;//joystick tanımlanırsa direction gelir ama bu sadece hareket etmesi gereken yönün vector3 değeridir. Bu değere göre hareket ettirmen gerek karakteri. keys: transform.translate veya character controller componenti (simplemove metoduyla birlikte)
 
     }
 
